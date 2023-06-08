5c7a889e13ed62efb8394b7a9a35da28d3490c88
Mattia222 <127505321+Mattia222@users.noreply.github.com>
2023-06-08 22:27:47 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,5 @@
-
+void init (char [], int);
+void init(char [][15]);
+void inserimento(char [][15], char [][15]);
+void inserimento(char [], int);
+int ricerca(char [][15], char [][15], char [], char []);
